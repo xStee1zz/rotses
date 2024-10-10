b6718153b30a0cdb4e9f4a4d6cc90930c136b403
xStee1zz <164562517+xStee1zz@users.noreply.github.com>
2024-10-10 17:19:56 +0300

    Create trit.py

---
+++ trit.py
@@ -0,0 +1,28 @@
+import asyncio
+import logging
+
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+logger = logging.getLogger(__name__)
+
+
+async def spam(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text('пiщов нахуй')
+
+
+async def main() -> None:
+    application = ApplicationBuilder().token('7804030886:AAFmqYAPW08gRlS6N6ASwqp5GXNPyifcS64').build()
+
+    application.add_handler(CommandHandler('spam', spam))
+
+    await application.run_polling()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
